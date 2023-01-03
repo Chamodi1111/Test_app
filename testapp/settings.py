12a118178dc185434e68e1244dc11761908fbc24
Chamodi1111 <92756394+Chamodi1111@users.noreply.github.com>
2023-01-03 10:09:30 +0530

    Update settings.py

--- testapp/settings.py
+++ testapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x^o19rkn6%$if^xo-f=b#-ffjn0gf^jye=waipbwhyn1kp@&5s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
