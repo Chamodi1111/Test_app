1e6c1036f85b24ca88897d230161b6cb080d8e07
Chamodi1111 <92756394+Chamodi1111@users.noreply.github.com>
2023-01-03 10:31:48 +0530

    Update settings.py

--- testapp/settings.py
+++ testapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x^o19rkn6%$if^xo-f=b#-ffjn0gf^jye=waipbwhyn1kp@&5s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [http://demo-lb-1026970636.us-east-1.elb.amazonaws.com/]
 
 
 # Application definition
